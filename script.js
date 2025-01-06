c92f38711eea87af533282528ce47383863b6bfb
sonia akter <soniaakterrotna90@gmail.com>
2025-01-06 13:29:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// const slider = document.querySelector('.breaking-slider');
+// const items = document.querySelectorAll('.breaking-item');
+// let currentIndex = 0;
+
+// function slideNews() {
+//   currentIndex++;
+//   if (currentIndex >= items.length) {
+//     currentIndex = 0; // Reset to first item
+//   }
+//   const offset = -currentIndex * items[0].offsetWidth; // Calculate offset
+//   slider.style.transform = `translateX(${offset}px)`; // Move slider
+// }
+
+// // Slide every 3 seconds
+// setInterval(slideNews, 3000);
